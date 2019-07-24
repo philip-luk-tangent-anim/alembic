6159aa20a62f02ece954528ea9b8bf4da64dab31
Philip Luk <philip.luk@tangent-animation.com>
2019-07-24 18:37:45 -0400

    Add setup_run_env.bat
    This is a script that just helps with setting up the environment for testing.  It's temporary until we implement the official REZ environment manager to handle the environment for building, running, deploying the code.

---
+++ setup_run_env.bat
@@ -0,0 +1,3 @@
+set PATH=C:\pipeline28\python\370_64;T:\Development\philip.luk\repo\local\thirdparty\openexr\2.3.0\win_x64\lib;T:\Development\philip.luk\repo\local\thirdparty\openexr\2.3.0\win_x64\bin;T:\Development\philip.luk\software\boost\1.68.0\win_x64\lib64-msvc-14.1;%PATH% 
+
+set PYTHONPATH=T:\Development\philip.luk\repo\local\thirdparty\openexr\2.3.0\win_x64\lib\python3.7\site-packages;T:\Development\philip.luk\repo\local\thirdparty\alembic\1.7.8\win_x64\lib\python3.7\site-packages;%PYTHONPATH% 
